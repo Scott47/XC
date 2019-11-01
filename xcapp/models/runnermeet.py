1e0e83aded989ef41f843213ec219b91fc7f5f18
scottsilver <scottsilver47@gmail.com>
2019-11-01 14:57:48 -0500

    changed related names on foreign keys on RunnerMeet model

--- xcapp/models/runnermeet.py
+++ xcapp/models/runnermeet.py
@@ -8,8 +8,8 @@ class RunnerMeet(models.Model):
     meet_time = models.FloatField(default=0)
     place = models.IntegerField()
     PR = models.BooleanField(null=True)
-    runner = models.ForeignKey(Runner, on_delete=models.DO_NOTHING, related_name='runner')
-    meet= models.ForeignKey(Meet, on_delete=models.DO_NOTHING, related_name='runnermeet')
+    runner = models.ForeignKey(Runner, on_delete=models.DO_NOTHING, related_name='runnermeet')
+    meet= models.ForeignKey(Meet, on_delete=models.DO_NOTHING, related_name='meetrunner')
 
     class Meta:
         verbose_name = ("runner_meet")
